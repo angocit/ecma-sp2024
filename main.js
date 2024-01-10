28fe97fd6b3d642ad3544f298145cae8d58b35e5
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-01-10 08:09:59 +0700

    lession 4

--- main.js
+++ main.js
@@ -40,7 +40,7 @@
 // let [so1,,,so4] = manga;
 // console.log(so4);
 // Spread, rest
-let manga = [1,2,100,10,5,6];
+// let manga = [1,2,100,10,5,6];
 // let max= manga[0];
 // for (let so of manga){
 //     if (max<so){
@@ -56,19 +56,64 @@ let manga = [1,2,100,10,5,6];
 // console.log(mangc);
 // let max = Math.min(...manga);
 // console.log(max);
-const result ={
-    success: ["max-length", "no-amd", "prefer-arrow-functions"],
-    failure: ["no-var", "var-on-top", "linebreak"],
-    skipped: ["no-extra-semi", "no-dup-keys"]
-    };
+// const result ={
+//     success: ["max-length", "no-amd", "prefer-arrow-functions"],
+//     failure: ["no-var", "var-on-top", "linebreak"],
+//     skipped: ["no-extra-semi", "no-dup-keys"]
+//     };
     
-function makeList(arr){
-    const failureItems =[] ; // Change the code here
-    for (let item of arr){
-        failureItems.push(`<li class="text-warning">${item}</li>`);
-    }
-    return failureItems;
-}
-const failuresList = makeList(result.failure);
-console.log(failuresList);
-    
+// function makeList(arr){
+//     const failureItems =[] ; // Change the code here
+//     for (let item of arr){
+//         failureItems.push(`<li class="text-warning">${item}</li>`);
+//     }
+//     return failureItems;
+// }
+// const failuresList = makeList(result.failure);
+// console.log(failuresList);
+// Falsy value; 
+// '',undefined,null,false,NaN,0
+// let sdt = 'Tiêu đề';
+// let sc = ""||0||null||'title'||10;
+
+// if (title==''){
+//   newtitle = 'Tiêu đề mặc định';
+// }
+// else {
+//   newtitle = title;
+// }
+
+// let mess = title||'Tiêu đề mặc định';
+// console.log(mess);
+// NUllish  ??
+
+// Optional Chaining
+// const person = {
+//       name: {fistname:"Ngô Bá",lastname:"Khá"},
+//       age: 27,
+//       address: "Bắc Ninh",
+//       status:"Juventus"
+//   }
+// let name = person.name?.lastname??'Tên không xác định';
+// console.log(name);
+// Set
+const manga = [5,6,2,5,6,4,6];
+// const temparr = [];
+// for (let item of manga) {
+//   if (temparr.indexOf(item)==-1){
+//     temparr.push(item);
+//   }
+// }
+// console.log(`Số phần tử có trong mảng là: ${temparr.length}`);
+const set1 = new Set(manga);
+// // set1.add(5);
+// // set1.add(6);
+// // set1.add(8);
+// // set1.delete(5);
+// const arrfilter=[];
+// for (let item of set1) {
+//   arrfilter.push(item);
+// }
+// console.log(manga);
+// console.log(arrfilter);
+console.log(`Số phần tử có trong mảng là: ${set1.size}`);

5f0ffd0560c16b589ebe386a4a06e5782a9d7aa7
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-01-12 08:33:21 +0700

    lession5

--- lession5.js
+++ lession5.js
@@ -1,9 +1,37 @@
-const tenham = function *(a,b){
-    yield a-b;
-    yield a+b;
-    yield a*b;
-}
-let ktra = tenham(5,6);
-console.log(`Hiệu là: ${ktra.next().value}`);
-console.log(`Tổng là: ${ktra.next().value}`);
-console.log(`Tích là: ${ktra.next().value}`);
+// const tenham = function *(a,b){
+//     yield a-b;
+//     yield a+b;
+//     yield a*b;
+// }
+// let ktra = tenham(5,6);
+// console.log(`Hiệu là: ${ktra.next().value}`);
+// console.log(`Tổng là: ${ktra.next().value}`);
+// console.log(`Tích là: ${ktra.next().value}`);
+// const obj = {
+//     firstname: "Ngô",
+//     lastname: "Khá",
+//     fullname(){
+//         return `${this.firstname} ${this.lastname}`;
+//     }
+// }
+// console.log(obj.fullname());
+// class Khuvuc{
+//     constructor(khuvuc,vitri){
+//         this.tenkhuvuc = khuvuc;
+//         this.vitri = vitri;
+//     }
+// }
+// class Quocgia extends Khuvuc{
+//     constructor(tenquocgia,dantoc,khuvuc,vitri){
+//         super(khuvuc,vitri);
+//         this.tenquocgia = tenquocgia;
+//         this.dantoc = dantoc;
+//     }
+// }
+// let kv = new Khuvuc("Đông Nam Á","Châu Á");
+// // console.log(`Khu vực: ${kv.tenkhuvuc}`);
+// let quocgia = new Quocgia("Vietnam","Kinh",kv.tenkhuvuc,kv.vitri);
+// console.log(`Ten quoc gia: ${quocgia.tenquocgia}`);
+// console.log(`Dan toc chu yeu: ${quocgia.dantoc}`);
+// console.log(`Thuộc khu vực: ${quocgia.tenkhuvuc}`);
+// console.log(`Tại vị trí: ${quocgia.vitri}`);

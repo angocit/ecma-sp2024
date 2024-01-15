7e9b4b4cc41f042cbfb8df4e5a2b5efe40c08731
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-01-15 07:49:40 +0700

    lession6

--- lession6.1.js
+++ lession6.1.js
@@ -1,4 +1,5 @@
-const a = [{a:6,b:9},{a:8,b:5},{a:3,b:6}];
+// const a = [{a:6,b:9},{a:8,b:5},{a:3,b:6}];
+const a = [5,6,7,8,9];
 // Map.
 // a.map((giatri,index)=>{
 //     console.log(giatri);
@@ -9,7 +10,10 @@ const a = [{a:6,b:9},{a:8,b:5},{a:3,b:6}];
 // });
 // let tong = a.reduce((result,giatri)=>result+=giatri);
 // console.log(tong);
-const b = a.filter(({a,b})=>{
-    return b>5;
+// const b = a.filter(({a,b})=>{
+//     return b>5;
+// });
+const b = a.filter((value)=>{
+    return value>6;
 });
 console.log(b);

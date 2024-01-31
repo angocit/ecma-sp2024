b258d64816cf9c26033e5e44d27ac26f7153a87a
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-01-31 08:27:58 +0700

    update cart

--- cart.js
+++ cart.js
@@ -40,7 +40,7 @@ const rederCart =async ()=>{
         <td> <input type="number" value="${item.quantity}" data-id="${item.pid}" onchange="ChangeQuantity(this)"/></td>
         <td>${product.price}</td>
         <td>${(product.price*item.quantity)}</td>
-        <td></td>
+        <td><a onclick="delProductInCart('${item.pid}')"><svg width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1 -32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1 -32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1 -32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.7 23.7 0 0 0 -21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0 -16-16z"/></svg></a></td>
         `;
         // Cộng dồn tiền mỗi lần lặp
         tongtien = tongtien+product.price*item.quantity;
@@ -83,6 +83,24 @@ const ChangeQuantity=(el)=>{
     cartArr[keyvalue].quantity = quantity;
     localStorage.setItem('cart',JSON.stringify(cartArr));
     // console.log(cartArr);
+    rederCart();
+}
+const  delProductInCart= (pid)=>{
+    // lấy thông tin giỏ hàng
+    const cart = localStorage.getItem('cart');
+    // chuyển string sang json
+    cartArr = JSON.parse(cart);
+    // Kiểm tra vị trí của sản phẩm muốn xóa
+    let keyvalue = -1; // Biến này để kiểm tra vị trí xuất hiện của sản phẩm trong giỏ hàng
+    cartArr.map((value,key)=>{
+        if (value.pid ==pid){ // Nếu sản phẩm có xuất trong giỏ hàng
+            keyvalue = key;
+        }
+    });
+    // Thực hiện xóa sản phẩm ở vị trí trả về
+    cartArr.splice(keyvalue,1);
+    // cập nhật lại giỏ hàng
+    localStorage.setItem('cart',JSON.stringify(cartArr));
     rederCart();
 }
 rederCart();

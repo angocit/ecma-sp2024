0459041ad64b2dc190c692d0e9cc7b8d2eec7e11
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-01-31 19:13:34 +0700

    update cart

--- cart2.js
+++ cart2.js
@@ -20,23 +20,33 @@ const rederCart = async()=>{
         // Duyệt mảng products để lấy thông tin id, số lượng kết hợp hàm getProductById để đổ dữ lieueh vào tbody
         // Truy cập tbody
         const tbody = document.querySelector('tbody');
-        products.map(async({pid,quantity},index)=>{
+        tbody.innerHTML = '';
+        let tong = 0;
+        // products.map(async({pid,quantity},index)=>{
+            let index = 0;
+            for (let item of products){
             // khởi tạo thẻ tr
             const tr = document.createElement('tr');
             // gọi sản phẩm theo id từ hàm getProductById
-            let product = await getProductById(pid);
+            let product = await getProductById(item.pid);
             tr.innerHTML=`
                 <td>${index+1}</td>
                 <td><img width="60" src="${product.image}"></td>
                 <td>${product.name}</td>
-                <td>${quantity}</td>
+                <td><input data="${item.pid}" type ="number" value="${item.quantity}" onchange="changeQuantity(this)"/></td>
                 <td>${product.price}</td>
-                <td>${product.price*quantity}</td>
+                <td>${product.price*item.quantity}</td>
                 <td>Xóa</td>
             `;
             // Đổ dữ liệu vào tbody
             tbody.appendChild(tr);
-        });
+            tong += product.price*item.quantity;
+            index++;
+        };
+        // truy cập node tổng tiền:
+        const total = document.querySelector('#totals span');
+        // console.log(total);
+        total.innerHTML =tong;
     }
 }
 const getProductById = async(pid)=>{
@@ -51,3 +61,27 @@ rederCart();
 //     console.log(product.name);
 // }
 // check();
+
+const changeQuantity = (el)=>{
+    // console.log(el);
+    // lấy thông tin số lượng và id sản phẩm
+    let pid = el.getAttribute('data');
+    let quantity = el.value;
+    // console.log(pid,quantity);
+    // Lấy thông tin giỏ hàng
+    const cart = localStorage.getItem('cart');
+    // chuyển dữ liệu giỏ hàng sang json
+    const cartArr = JSON.parse(cart)
+    // Kiểm tra vị trí xuất hiện của sản phẩm trong giỏ hàng
+    let keyvalue = -1; // Biến này để kiểm tra vị trí xuất hiện của sản phẩm nếu nó tồn tại trong giỏ hàng
+    cartArr.map((value,key)=>{
+        if (value.pid ==pid){ // Nếu sản phẩm có xuất trong giỏ hàng
+            keyvalue = key;
+        }
+    });
+    // Thay đổi số lượng tại vị trí sản phẩm xuất hiện
+    cartArr[keyvalue].quantity = quantity;
+    // Set lại giỏ hàng theo giá trị mới
+    localStorage.setItem('cart',JSON.stringify(cartArr));
+    rederCart();
+}
---
+++ checkout.js
@@ -0,0 +1,90 @@
+const getProductById=async (pid)=>{
+    product = await fetch(`http://localhost:3000/products/${pid}`); //Gọi api
+    product = await product.json();
+    // console.log(product);
+    return {name: product.name,price:product.price,image:product.image}
+}
+const rederCart =async ()=>{
+    // Lấy dữ liệu giỏ hàng từ localStorage.
+    //  - Kiểm tra nếu tồn tại thì xử lý render cart.
+    //  - Nếu không tồn tại thì thông báo là giỏ hàng trống
+    const cart  = localStorage.getItem('cart');
+    if (cart ==null){ // Giỏ hàng trống
+        // THông báo là giỏ hàng trống.
+        // - Truy cập đến node muốn hiển thị thông báo.
+        const mess = document.getElementById("cart");
+        // Đổ thông báo
+        mess.innerHTML="Giỏ hàng trống";
+    }
+    else { // Giỏ hàng có sản phẩm
+        const products = JSON.parse(cart); // Chuyển dữ liệu lấy từ localstorage sang object
+        // console.log(products);
+        // Truy cập đến vị trí node muốn đổ dữ liệu (tbody).
+        const tbody = document.querySelector("tbody");
+        tbody.innerHTML='';
+        // Duyệt mảng products
+        var tongtien = 0;
+        for (let item of products){
+            // console.log(pid,quantity); // Chỉ có id sản phẩm và số lượng
+            // Mong muốn là phải có cả tên, ảnh, đơn giá sp
+        //  =>Cần viết hàm lấy thông tin tên, ảnh, đơn giá từ id sản phẩm. getProductById()
+        
+        // lấy dữ liệu sản phẩm thông qua id sản phẩm``
+        const product = await getProductById(item.pid);
+        // Tạo một node con chứa thông tin sản phẩm
+
+        const tr = document.createElement('tr');
+        //Đổ dữ liệu html vào node con.
+        tr.innerHTML=`
+        <td></td>
+        <td><img src ="${product.image}" width="60"/></td>
+        <td>${product.name}</td>
+        <td>${item.quantity}</td>
+        <td>${(product.price*item.quantity)}</td>
+         `;
+        // Cộng dồn tiền mỗi lần lặp
+        tongtien = tongtien+product.price*item.quantity;
+        console.log(tongtien);
+        // Đổ dữ liệu của node con vào node tbody
+        tbody.appendChild(tr);
+            // console.log(product.name);
+            //
+        };
+        // Đổ tổng tiền vào node tổng tiền. => truy cập vào node tổng tiền.
+        console.log(tongtien);
+        const total = document.querySelector('#total span');
+        // Đổ dữ liệu tổng tiền vào node
+        total.innerHTML = tongtien;
+    }
+}
+const checkOut=async()=>{
+    event.preventDefault();
+    let hoten = document.querySelector('input[name="name"]');
+    let diachi = document.querySelector('input[name="address"]');
+    let sdt = document.querySelector('input[name="phone"]');
+    let status = "Đang xử lý";
+    let created_date = new Date();
+    // Update thoogn tin khách hàng vào trong order đồng thời update thông tin đơn hàng vào mục order_details
+    // - Update thông tin khahs hàng hangfbangr order
+    const order = await fetch('http://localhost:3000/order',
+    {
+        method: 'POST',
+        body: JSON.stringify({hoten,diachi,sdt,status,created_date})
+    }
+    );
+    // Lấy ID của order
+    const order_id = await order.json();
+    // Lấy thông tin giỏ hàng lưu vào order_details.
+    let cart = localStorage.getItem('cart');
+    cart = JSON.parse(cart);
+    for (let item of cart) {
+        // Insert từng sna rphamar vào order_details
+        await fetch('http://localhost:3000/order_details',
+            {
+                method: 'POST',
+                body: JSON.stringify({order_id:order_id.id,product_id:item.pid,quantity:item.quantity})
+            }
+            );
+    }
+}
+rederCart();
